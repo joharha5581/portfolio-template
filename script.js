b3594cdfc0855c32fb35516f3dea56db23720e5c
joharha5581 <joharha@nycstudents.net>
2025-09-30 08:43:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"; // update the part BETWEEN the "quotes"
-let userName = "YourUsername"; // same here
+let ownerName = "Joharh A"; // update the part BETWEEN the "quotes"
+let userName = "joharha5581"; // same here
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
